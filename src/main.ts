39b9082f0c404b5473c8d76a60bff566689c3a79
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2020-12-09 06:51:59 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.REDIS,
+      options: {
+        url: 'redis://localhost:6379',
+      },
+    },
+  );
+  app.listen(() => {
+    Logger.log('Microservice is listening!');
+  });
 }
 bootstrap();
